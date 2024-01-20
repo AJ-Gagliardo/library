cdeaf002e8bf66ffd5be8736818c6541726da28c
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-01-20 15:46:30 -0800

    added a addBookToLibrary function to automatically add the books to the library array, this will be useful alter on

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ basic UI
 myLibrary where i save all teh books in an array
 object constructor (function) for books, so i can keep inserting new books
 test the object construcotr on the console
+function to input info from the book and then  push the new books to the library
 
 
 UI where the user can put the informaiton of the book to the function/constructor
@@ -25,5 +26,17 @@ function Book(title, author, pages, read  ){
 
 }
 
-book1 = new Book('Subtle Art of Not Giving a F**', 'Mark Manson', 224, true)
-console.log(book1)
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
+    //maybe here i can do DOM to put all myLibrary[i] in the grid '#content' 
+
+  }
+
+// book1 = new Book('Subtle Art of Not Giving a F**', 'Mark Manson', 224, true)
+// console.log(book1)
+
+// myLibrary.push(book1);
+
+const content = document.getElementById('content');
+console.log(content);
+

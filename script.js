4a81d1a85a3d624ed857a1fe0087463bed929c26
Antonio Gagliardo <ajgagliardo92@gmail.com>
2024-01-22 01:06:22 -0800

    changed my approach and this time the display will add 1 book at a time instead of going through every book

--- script.js
+++ script.js
@@ -94,6 +94,70 @@ function DisplayLibrary(){
 }
 */
 
+
+
+function resetDisplayLibrary(){
+    // myLibrary.length=0;
+    const allBooks = document.querySelectorAll('.book');
+    allBooks.forEach(book => {
+        book.remove();
+    });
+    
+    
+    }
+    
+    
+    function addDisplayLibrary(){
+
+    
+            // This section is to create div for each book, so the data can be inserted in the grid
+            const bookDiv = document.createElement('div');
+            bookDiv.classList.add('book'); // this is to separate books in grid
+            bookDiv.id= `book${myLibrary[myLibrary.length-1]}`;   // not sure if I will use it but can be helpful to have individual ids
+        // This next section is for each aprt of the book, will be useful later for CSS (if i decide to do different colors to each book presentation);
+            const titleDiv=document.createElement('div');
+            titleDiv.classList.add('title');
+            titleDiv.textContent= `${myLibrary[myLibrary.length-1].title}`
+    
+            const authorDiv=document.createElement('div');
+            authorDiv.classList.add('author');
+            authorDiv.textContent= `${myLibrary[myLibrary.length-1].author}`;
+    
+            const pagesDiv=document.createElement('div');
+            pagesDiv.classList.add('pages');
+            pagesDiv.textContent= `${myLibrary[myLibrary.length-1].pages}`;
+    
+            const readDiv = document.createElement('div');
+           
+           if (myLibrary[myLibrary.length-1].read === true){
+    
+               readDiv.classList.add('read');
+               readDiv.textContent = "read";
+           }
+               else { 
+    
+                   readDiv.classList.add('notRead');
+                   readDiv.textContent = "Not Read";
+                }
+                 
+    
+    
+    
+        // putting elements inside the book div
+            bookDiv.appendChild(titleDiv);
+            bookDiv.appendChild(authorDiv);
+            bookDiv.appendChild(pagesDiv);
+            bookDiv.appendChild(readDiv);
+    
+            content.appendChild(bookDiv);
+    
+    
+        ;
+    }
+
+
+
+
 //###### Important, i may be doing wrongapproach on oing through ALL of the elements everytime I create a new one
 // maybe i should just add 1 by 1 and then  when i want to eras ethem just do it y ID. and when adding probably can do with .length to get the last one
 
@@ -104,7 +168,8 @@ function addBookToLibrary(title, author, pages, read) {
     // content.appendChild(bookDiv);
     // content.appendChild(myLibrary[0])
 // run a for loop myLibrary[i] 
-DisplayLibrary();
+// DisplayLibrary();
+addDisplayLibrary();
 // by ${book[0].author}, has ${book[0].pages}
   }
 
